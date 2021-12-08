d46ddd005cd08949fab0f51dd471cac2205df53a
Massimo Melina <a@rejetto.com>
2021-12-08 14:50:09 +0100

    switched to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-const Koa = require('koa')
-const serve = require('koa-static')
-const srv = new Koa()
+import Koa from 'koa'
+import serve from 'koa-static'
 
+const srv = new Koa()
 const frontend = serve('frontend')
 srv.use((ctx,next) => {
     const rq = ctx.request
     if (rq.method === 'GET' && rq.originalUrl === '/c') {
-        ctx.body = 'ciao'
+        ctx.body = 'ciao3'
     }
     else
         return frontend(ctx,next)
 })
-srv.listen(3000)
-console.log('running')
+srv.on('error', err => console.error('server error', err))
+srv.listen(3000, ()=> console.log('running'))
 /*
 body
 set(k,v)
