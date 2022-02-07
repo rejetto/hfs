6249f04d34239e8eaec5f5bc5c26ec14038af681
Massimo Melina <a@rejetto.com>
2022-02-07 10:20:30 +0100

    fix: link in admin/home not working if not localhost

--- admin/src/HomePage.ts
+++ admin/src/HomePage.ts
@@ -8,7 +8,7 @@ export default function HomePage() {
     const { http, https } = res || {}
     const secure = !http?.active && https?.active ? 's' : ''
     const srv = secure ? https : (http?.active && http)
-    const href = srv && `http${secure}://localhost`+ (srv.port === (secure ? 443 : 80) ? '' : ':'+srv.port)
+    const href = srv && `http${secure}://`+window.location.hostname + (srv.port === (secure ? 443 : 80) ? '' : ':'+srv.port)
     return h(Box, { flex: 1 },
         h(Typography, { variant: 'h4' },
             !res ? spinner()
