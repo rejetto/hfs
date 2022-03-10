21a533848919b822c9d825a1ee2e279546530b26
Massimo Melina <a@rejetto.com>
2022-03-10 18:51:17 +0100

    updater plugin (exe version only)

---
+++ plugins/updater-disabled/plugin.js
@@ -0,0 +1,42 @@
+/*
+this plugin is currently working only with exe version.
+Its capability is to offer automatic restart when an update is available in the form of hfs.exe-new file.
+ */
+const NEW = 'hfs.exe-new'
+const BATCH_NAME = 'run-updater.bat'
+const BATCH = `@echo off
+:loop
+setlocal
+SET hfs_updater=1
+hfs
+endlocal
+if exist hfs.exe-new (
+  move /y hfs.exe hfs.exe-old
+  move /y ${NEW} hfs.exe
+  goto loop  
+)
+`
+
+const LOG_PREFIX = "UPDATER:"
+
+exports.init = async api => {
+    const fs = api.require('fs')
+    fs.writeFile(BATCH_NAME, BATCH, err =>
+        err && console.error(LOG_PREFIX, "could'nt write", BATCH_NAME))
+    if (!process.env.hfs_updater)
+        return console.log(LOG_PREFIX, "run", BATCH_NAME, "to have restart-on-update")
+
+    console.log(LOG_PREFIX, "ready")
+    const timer = setInterval(() => {
+        fs.access(NEW, err => {
+            if (err) return
+            console.log(LOG_PREFIX, "exiting for update")
+            process.exit(0)
+        })
+    }, 5000)
+    return {
+        unload() {
+            clearInterval(timer)
+        }
+    }
+}
