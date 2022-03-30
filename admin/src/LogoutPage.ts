bfd84b8cc4ebb23f311d87bd176102f6e831c3bc
Massimo Melina <a@rejetto.com>
2022-03-30 17:02:14 +0200

    fix: admin/logout: old message

--- admin/src/LogoutPage.ts
+++ admin/src/LogoutPage.ts
@@ -9,7 +9,7 @@ export default function LogoutPage() {
     const { username } = useSnapState()
     if (!cfg) return null
     if (!username)
-        return h(Alert, { severity: 'info' }, "You are not logged in, because authentication is not currently required. You can enable it in the Configuration page.")
+        return h(Alert, { severity: 'info' }, "You are not logged in, because authentication is not required on localhost")
     return h(Box, { display: 'flex', flexDirection:'column', gap: 2 },
         "You are logged in as " + username,
         h(Box, {},
