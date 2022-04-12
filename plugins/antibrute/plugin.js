1ba07effe50716bd568216ceb2b876321c958245
Massimo Melina <a@rejetto.com>
2022-04-12 21:22:43 +0200

    fix: antibrute plugin not working in exe distribution

--- plugins/antibrute/plugin.js
+++ plugins/antibrute/plugin.js
@@ -1,5 +1,3 @@
-const { API_URI } = require('@hfs/server/src/const')
-
 exports.version = 1
 exports.description = "Introduce increasing delays between login attempts."
 
@@ -7,26 +5,29 @@ exports.description = "Introduce increasing delays between login attempts."
 const INCREMENT = 5_000
 const CAP = 60_000
 
-const LOGIN_URI = API_URI + 'loginSrp1'
 const byIp = {}
 
-exports.init = api => ({
-    async middleware(ctx) {
-        if (ctx.path !== LOGIN_URI) return
-        const k = ctx.ip
-        const now = Date.now()
-        const rec = byIp[k]
-        if (rec) {
-            const wait = rec.when - now
-            if (wait > 0) {
-                console.log('plugin antibrute is delaying', k, 'for', Math.round(wait/1000))
-                await new Promise(resolve => setTimeout(resolve, wait))
+exports.init = api => {
+    const { API_URI } = api.require(api.srcDir + '/const')
+    const LOGIN_URI = API_URI + 'loginSrp1'
+    return ({
+        async middleware(ctx) {
+            if (ctx.path !== LOGIN_URI) return
+            const k = ctx.ip
+            const now = Date.now()
+            const rec = byIp[k]
+            if (rec) {
+                const wait = rec.when - now
+                if (wait > 0) {
+                    console.log('plugin antibrute is delaying', k, 'for', Math.round(wait / 1000))
+                    await new Promise(resolve => setTimeout(resolve, wait))
+                }
+                ctx.set('x-anti-brute-force', wait)
             }
-            ctx.set('x-anti-brute-force', wait)
+            const delay = Math.min(CAP, (rec?.delay || 0) + INCREMENT)
+            byIp[k] = { delay, when: now + delay }
+            setTimeout(() => delete byIp[k], delay * 10) // no memory leak
         }
-        const delay = Math.min(CAP, (rec?.delay || 0) + INCREMENT)
-        byIp[k] = { delay, when: now + delay }
-        setTimeout(() => delete byIp[k], delay * 10) // no memory leak
-    }
-})
+    })
+}
 
