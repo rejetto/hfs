655cb3460f37d977cd89e3946ef21bb4131a1ad1
Massimo Melina <a@rejetto.com>
2022-04-27 16:00:46 +0200

    plugin/vhosting: use same format for fields as for exports.config. Mixing can be confusing for who's reading.

--- plugins/vhosting/plugin.js
+++ plugins/vhosting/plugin.js
@@ -3,25 +3,30 @@ exports.version = 1
 exports.apiRequired = 2 // 2 is for the config 'array'
 
 exports.config = {
-    hosts: { label: '', type: 'array', height: 300, fields: [
-        { k: 'host', label: "Domain" },
-        { k: 'path', helperText: "Root path in VFS" },
-    ]  }
+    hosts: {
+        label: '',
+        type: 'array',
+        fields: {
+            host: { label: "Domain" },
+            root: { helperText: "Root path in VFS" },
+        },
+        height: 300,
+    }
 }
 
 exports.init = api => ({
     middleware(ctx) {
         let toModify = ctx
-        if (ctx.path.startsWith(api.const.SPECIAL_URI)) {
+        if (ctx.path.startsWith(api.const.SPECIAL_URI)) { // special uris should be excluded...
             toModify = ctx.request.query
-            if (toModify.path === undefined)
+            if (toModify.path === undefined) // ...unless they carry a path in the query. In that case we'll work that.
                 return
         }
         const hosts = api.getConfig('hosts')
         if (!hosts) return
         for (const row of hosts)
             if (ctx.host === row.host) {
-                toModify.path = row.path + toModify.path
+                toModify.path = row.root + toModify.path
                 return
             }
     }
