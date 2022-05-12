f3336a87b038635986da5e1d6b3669ad3bfdadcc
Massimo Melina <a@rejetto.com>
2022-05-13 00:28:03 +0200

    support \n in md()

--- admin/src/md.ts
+++ admin/src/md.ts
@@ -4,20 +4,25 @@ import { createElement as h, Fragment } from 'react'
 
 // markdown inspired syntax to transform text into react elements: * for bold, / for italic, _ for underline, ` for code
 export default function md(text: string) {
-    const re = /([`*/_])(.+)\1/g
+    const re = /([`*/_])(.+)\1|(\n)/g
     const res = []
     let last = 0
     let match
     while (match = re.exec(text)) { //eslint-disable-line no-cond-assign
-        const tag = ({
-            '`': 'code',
-            '*': 'b',
-            '/': 'i',
-            '_': 'u',
-        })[ match[1] ]
-        if (!tag)
-            throw Error("should never happen")
-        res.push( text.slice(last, match.index), h(tag,{}, match[2]) )
+        res.push( text.slice(last, match.index) )
+        if (match[3])
+            res.push(h('br'))
+        else {
+            const tag = ({
+                '`': 'code',
+                '*': 'b',
+                '/': 'i',
+                '_': 'u',
+            })[ match[1] ]
+            if (!tag)
+                throw Error("should never happen")
+            res.push( h(tag,{}, match[2]) )
+        }
         last = match.index + match[0].length
     }
     return h(Fragment, {}, ...res, text.slice(last, Infinity))
