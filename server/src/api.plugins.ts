1c1f8c2f25db1ed6caf9341d523067c42388f230
Massimo Melina <a@rejetto.com>
2022-05-14 14:48:28 +0200

    better code: split files

--- server/src/api.plugins.ts
+++ server/src/api.plugins.ts
@@ -4,20 +4,16 @@ import {
     getAvailablePlugins,
     getPluginConfigFields,
     mapPlugins,
-    parsePluginSource,
     Plugin, pluginsConfig,
-    PATH as PLUGINS_PATH, isPluginRunning, enablePlugin, getPluginInfo, rescan
+    PATH as PLUGINS_PATH, isPluginRunning, enablePlugin, getPluginInfo
 } from './plugins'
 import _ from 'lodash'
 import assert from 'assert'
-import { httpsString, httpsStream, objSameKeys, onOff, same, wait } from './misc'
-import { ApiError, ApiHandlers, sendList } from './apiMiddleware'
+import { objSameKeys, onOff, same, wait } from './misc'
+import { ApiHandlers, sendList } from './apiMiddleware'
 import events from './events'
-import unzipper from 'unzipper'
-import { createWriteStream, mkdirSync } from 'fs'
 import { rm } from 'fs/promises'
-
-const DIST_ROOT = 'dist/'
+import { downloadPlugin, getRepo2folder, getRepoInfo, readOnlinePlugin, searchPlugins } from './github'
 
 const apis: ApiHandlers = {
 
@@ -86,62 +82,42 @@ const apis: ApiHandlers = {
     search_online_plugins({ text }, ctx) {
         const list = sendList()
         const repo2folder = getRepo2folder()
-        // do our job in then() so api can return in the meantime
-        apiGithub('search/repositories?q=topic:hfs-plugin+' + encodeURI(text)).then(async res => {
-            for (const it of res.items) {
-                const repo = it.full_name
-                let pl = await readOnlinePlugin(it)
-                if (!pl.apiRequired) continue // mandatory field
-                if (pl.badApi) { // we try other branches (starting with 'api')
-                    const branches: string[] = (await apiGithub('repos/' + it.full_name + '/branches'))
-                        .map((x:any) => x.name).filter((x: string) => x.startsWith('api')).sort()
-                    for (const branch of branches) {
-                        pl = await readOnlinePlugin(it, branch)
-                        if (!pl.apiRequired)
-                            pl.badApi = '-'
-                        if (!pl.badApi)
-                            break
-                    }
+        setTimeout(async () => {
+            try {
+                for await (const pl of searchPlugins(text)) {
+                    const repo = pl.id
+                    Object.assign(pl, { installed: repo2folder[repo] })
+                    list.add(pl)
+                    // watch for events about this plugin, until this request is closed
+                    ctx.req.on('close', onOff(events, {
+                        pluginInstalled: p => {
+                            if (p.repo === repo)
+                                list.update({ id: repo }, { installed: true })
+                        },
+                        pluginUninstalled: id => {
+                            if (repo === _.findKey(repo2folder, x => x === id))
+                                list.update({ id: repo }, { installed: false })
+                        },
+                        ['pluginDownload_'+repo](status) {
+                            list.update({ id: repo }, { downloading: status ?? null })
+                        }
+                    }) )
                 }
-                if (pl.badApi)
-                    continue
-                Object.assign(pl, { // inject some extra useful fields
-                    downloading: downloading[repo],
-                    installed: repo2folder[repo]
-                })
-                list.add(pl)
-                // watch for events about this plugin, until this request is closed
-                ctx.req.on('close', onOff(events, {
-                    pluginInstalled: p => {
-                        if (p.repo === repo)
-                            list.update({ id: repo }, { installed: true })
-                    },
-                    pluginUninstalled: id => {
-                        if (repo === _.findKey(repo2folder, x => x === id))
-                            list.update({ id: repo }, { installed: false })
-                    },
-                    ['pluginDownload_'+repo](status) {
-                        list.update({ id: repo }, { downloading: status ?? null })
-                    }
-                }) )
+            }
+            catch (err: any) {
+                list.error(err.code || err.message)
             }
             list.end()
-        }, (err: any) => {
-            list.error(err.code || err.message)
         })
         return list.return
     },
 
     async download_plugin(pl) {
-        if (downloading[pl.id])
-            return new ApiError(409, "already downloading")
         await downloadPlugin(pl.id, pl.branch)
         return {}
     },
 
     async update_plugin(pl) {
-        if (downloading[pl.id])
-            return new ApiError(409, "already downloading")
         await downloadPlugin(pl.id, pl.branch, true)
         return {}
     },
@@ -157,80 +133,4 @@ const apis: ApiHandlers = {
 
 }
 
-type DownloadStatus = true | undefined
-const downloading: Record<string, DownloadStatus> = {}
-
-function downloadProgress(id: string, status: DownloadStatus) {
-    if (status === undefined)
-        delete downloading[id]
-    else
-        downloading[id] = status
-    events.emit('pluginDownload_'+id, status)
-}
-
-async function downloadPlugin(repo: string, branch='', overwrite?: boolean) {
-    downloadProgress(repo, true)
-    const rec = await getRepoInfo(repo)
-    if (!branch)
-        branch = rec.default_branch
-    const url = `https://github.com/${repo}/archive/refs/heads/${branch}.zip`
-    const res = await httpsStream(url)
-    const repo2 = repo.split('/')[1] // second part, repo without the owner
-    const repo2clash = !overwrite
-        && (getAvailablePlugins().find(x => x.id === repo2) || mapPlugins(x => x.id === repo2).some(Boolean))
-    const pluginFolder = repo2clash ? repo.replace('/','-') : repo2 // longer form only if necessary
-    const installFolder = PLUGINS_PATH + '/' + pluginFolder
-    const GITHUB_ZIP_ROOT = repo2 + '-' + (branch) // github puts everything within this folder
-    const rootWithinZip = GITHUB_ZIP_ROOT + '/' + DIST_ROOT
-    return new Promise(resolve =>
-        res.pipe(unzipper.Parse())
-            .on('entry', entry => {
-                const { path, type } = entry
-                if (!path.startsWith(rootWithinZip))
-                    return entry.autodrain()
-                const dest = installFolder + '/' + path.slice(rootWithinZip.length)
-                if (type === 'File')
-                    return entry.pipe(createWriteStream(dest))
-                mkdirSync(dest, { recursive: true }) // easy way be sure to have the folder ready before proceeding
-            })
-            .on('close', () => {
-                rescan() // workaround: for some reason, operations above are not triggering the rescan of the watched folder. Let's invoke it.
-                resolve(undefined)
-                downloadProgress(repo, undefined)
-            }))
-}
-
 export default apis
-
-async function apiGithub(uri: string) {
-    const res = await httpsString('https://api.github.com/'+uri, {
-        headers: {
-            'User-Agent': 'HFS',
-            Accept: 'application/vnd.github.v3+json',
-        }
-    })
-    if (!res.ok)
-        throw res.statusCode
-    return JSON.parse(res.body)
-}
-
-function getRepoInfo(id: string) {
-    return apiGithub('repos/'+id)
-}
-
-async function readOnlinePlugin(repoInfo: { full_name: string, default_branch: string }, branch='') {
-    const url = `https://raw.githubusercontent.com/${repoInfo.full_name}/${branch || repoInfo.default_branch}/${DIST_ROOT}plugin.js`
-    const res = await httpsString(url)
-    if (!res.ok)
-        throw res.statusCode
-    const pl = parsePluginSource(repoInfo.full_name, res.body) // use 'repo' as 'id' client-side
-    pl.branch = branch || undefined
-    return pl
-}
-
-function getRepo2folder() {
-    const ret = Object.fromEntries(getAvailablePlugins().map(x => [x.repo, x.id]))
-    Object.assign(ret, Object.fromEntries(mapPlugins(x => [x.getData().repo, x.id]))) // started ones
-    delete ret.undefined
-    return ret
-}
---
+++ server/src/github.ts
@@ -0,0 +1,112 @@
+import events from './events'
+import { httpsStream, httpsString } from './misc'
+import { getAvailablePlugins, mapPlugins, parsePluginSource, PATH as PLUGINS_PATH, rescan } from './plugins'
+import unzipper from 'unzipper'
+import { createWriteStream, mkdirSync } from 'fs'
+import { ApiError } from './apiMiddleware'
+
+const DIST_ROOT = 'dist/'
+
+type DownloadStatus = true | undefined
+const downloading: Record<string, DownloadStatus> = {}
+
+function downloadProgress(id: string, status: DownloadStatus) {
+    if (status === undefined)
+        delete downloading[id]
+    else
+        downloading[id] = status
+    events.emit('pluginDownload_'+id, status)
+}
+
+export async function downloadPlugin(repo: string, branch='', overwrite?: boolean) {
+    if (downloading[repo])
+        return new ApiError(409, "already downloading")
+    downloadProgress(repo, true)
+    const rec = await getRepoInfo(repo)
+    if (!branch)
+        branch = rec.default_branch
+    const url = `https://github.com/${repo}/archive/refs/heads/${branch}.zip`
+    const res = await httpsStream(url)
+    const repo2 = repo.split('/')[1] // second part, repo without the owner
+    const repo2clash = !overwrite
+        && (getAvailablePlugins().find(x => x.id === repo2) || mapPlugins(x => x.id === repo2).some(Boolean))
+    const pluginFolder = repo2clash ? repo.replace('/','-') : repo2 // longer form only if necessary
+    const installFolder = PLUGINS_PATH + '/' + pluginFolder
+    const GITHUB_ZIP_ROOT = repo2 + '-' + (branch) // github puts everything within this folder
+    const rootWithinZip = GITHUB_ZIP_ROOT + '/' + DIST_ROOT
+    return new Promise(resolve =>
+        res.pipe(unzipper.Parse())
+            .on('entry', entry => {
+                const { path, type } = entry
+                if (!path.startsWith(rootWithinZip))
+                    return entry.autodrain()
+                const dest = installFolder + '/' + path.slice(rootWithinZip.length)
+                if (type === 'File')
+                    return entry.pipe(createWriteStream(dest))
+                mkdirSync(dest, { recursive: true }) // easy way be sure to have the folder ready before proceeding
+            })
+            .on('close', () => {
+                rescan() // workaround: for some reason, operations above are not triggering the rescan of the watched folder. Let's invoke it.
+                resolve(undefined)
+                downloadProgress(repo, undefined)
+            }))
+}
+
+export function getRepoInfo(id: string) {
+    return apiGithub('repos/'+id)
+}
+
+export async function readOnlinePlugin(repoInfo: { full_name: string, default_branch: string }, branch='') {
+    const url = `https://raw.githubusercontent.com/${repoInfo.full_name}/${branch || repoInfo.default_branch}/${DIST_ROOT}plugin.js`
+    const res = await httpsString(url)
+    if (!res.ok)
+        throw res.statusCode
+    const pl = parsePluginSource(repoInfo.full_name, res.body) // use 'repo' as 'id' client-side
+    pl.branch = branch || undefined
+    return pl
+}
+
+export function getRepo2folder() {
+    const ret = Object.fromEntries(getAvailablePlugins().map(x => [x.repo, x.id]))
+    Object.assign(ret, Object.fromEntries(mapPlugins(x => [x.getData().repo, x.id]))) // started ones
+    delete ret.undefined
+    return ret
+}
+
+async function apiGithub(uri: string) {
+    const res = await httpsString('https://api.github.com/'+uri, {
+        headers: {
+            'User-Agent': 'HFS',
+            Accept: 'application/vnd.github.v3+json',
+        }
+    })
+    if (!res.ok)
+        throw res.statusCode
+    return JSON.parse(res.body)
+}
+
+export async function* searchPlugins(text: string) {
+    const res = await apiGithub('search/repositories?q=topic:hfs-plugin+' + encodeURI(text))
+    for (const it of res.items) {
+        const repo = it.full_name
+        let pl = await readOnlinePlugin(it)
+        if (!pl.apiRequired) continue // mandatory field
+        if (pl.badApi) { // we try other branches (starting with 'api')
+            const branches: string[] = (await apiGithub('repos/' + it.full_name + '/branches'))
+                .map((x: any) => x.name).filter((x: string) => x.startsWith('api')).sort()
+            for (const branch of branches) {
+                pl = await readOnlinePlugin(it, branch)
+                if (!pl.apiRequired)
+                    pl.badApi = '-'
+                if (!pl.badApi)
+                    break
+            }
+        }
+        if (pl.badApi)
+            continue
+        Object.assign(pl, { // inject some extra useful fields
+            downloading: downloading[repo],
+        })
+        yield pl
+    }
+}
