78861a56ca7eff01553c34ee253750e17a005aaa
Massimo Melina <a@rejetto.com>
2022-06-29 10:32:34 +0200

    fix: error entering CLI command

--- server/src/commands.ts
+++ server/src/commands.ts
@@ -1,17 +1,17 @@
 import { addAccount, getAccount, updateAccount } from './perm'
 import { getConfigDefinition, setConfig } from './config'
+import _ from 'lodash'
 
 console.log(`HINT: type "help" for help`)
 require('readline').createInterface({ input: process.stdin }).on('line', (line: string) => {
-    const [command, ...params] = line.split(/ +/)
-    const fun = (commands as any)[command]
-    if (!fun)
-        return console.error("cannot understand entered command")
-    if (fun.length > params.length) {
-        const [args] = /\((.+)\)\s*\{/.exec(fun)!
-        return console.error("insufficient parameters, expected: " + args)
-    }
-    fun(...params).then(() =>console.log("command executed"),
+    if (!line) return
+    const [name, ...params] = line.split(/ +/)
+    const cmd = (commands as any)[name]
+    if (!cmd)
+        return console.error("cannot understand entered command, try 'help'")
+    if (cmd.cb.length > params.length)
+        return console.error("insufficient parameters, expected: " + cmd.params)
+    cmd.cb(...params).then(() =>console.log("command executed"),
         (err: any) => {
             if (typeof err === 'string')
                 console.error("command failed:", err)
@@ -20,36 +20,61 @@ require('readline').createInterface({ input: process.stdin }).on('line', (line: 
         })
 })
 
+function getFunctionArgs(fun: Function) {
+    const args = /\((.*)\)\s*\{/.exec(fun.toString())![1]
+    return args && args.split(',').map(arg => {
+        const [name,def] = arg.split('=')
+        return `<${name.trim()}>${def ? '='+def.trim() : ''}`
+    }).join(' ')
+}
+
 const commands = {
-    async help() {
-        console.log("supported commands:", ...Object.keys(commands).map(x => '\n - ' + x))
+    help: {
+        params: '',
+        async cb() {
+            console.log("supported commands:",
+                ..._.map(commands, ({ params }, name) =>
+                    '\n - ' + name + ' ' + params))
+        }
     },
-    async 'create-admin'(password: string, username='admin') {
-        if (getAccount(username))
-            throw `user ${username} already exists`
-        const acc = addAccount(username, { admin: true })
-        await updateAccount(acc!, acc => {
-            acc.password = password
-        })
+    'create-admin': {
+        params: '<password> [<username>=admin]',
+        async cb(password: string, username='admin') {
+            if (getAccount(username))
+                throw `user ${username} already exists`
+            const acc = addAccount(username, { admin: true })
+            await updateAccount(acc!, acc => {
+                acc.password = password
+            })
+        }
     },
-    async 'change-password'(user: string, password: string) {
-        const acc = getAccount(user)
-        if (!acc)
-            throw "user doesn't exist"
-        await updateAccount(acc!, acc => {
-            acc.password = password
-        })
+    'change-password': {
+        params: '<user> <password>',
+        async cb(user: string, password: string) {
+            const acc = getAccount(user)
+            if (!acc)
+                throw "user doesn't exist"
+            await updateAccount(acc!, acc => {
+                acc.password = password
+            })
+        }
     },
-    async config(key: string, value: string) {
-        const conf = getConfigDefinition(key)
-        if (!conf)
-            throw "specified key doesn't exist"
-        let v: any = value
-        try { v = JSON.parse(v) }
-        catch {}
-        setConfig({ [key]: v })
+    config: {
+        params: '<key> <value>',
+        async cb(key: string, value: string) {
+            const conf = getConfigDefinition(key)
+            if (!conf)
+                throw "specified key doesn't exist"
+            let v: any = value
+            try { v = JSON.parse(v) }
+            catch {}
+            setConfig({ [key]: v })
+        }
     },
-    async quit() {
-        process.exit(0)
+    quit: {
+        params: '',
+        async cb() {
+            process.exit(0)
+        }
     }
 }
