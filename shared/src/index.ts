07531fb8be5bf8928053b80fd57657384c6adff0
Massimo Melina <a@rejetto.com>
2022-08-29 14:44:34 +0200

    fix: let tests run without requiring 'dom' among typescript's libs

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,9 +1,9 @@
 // This file is part of HFS - Copyright 2021-2022, Massimo Melina <a@rejetto.com> - License https://www.gnu.org/licenses/gpl-3.0.txt
 
 import _ from 'lodash'
-import { SRPClientSession, SRPParameters, SRPRoutines } from 'tssrp6a'
 export * from './react'
 export * from './dialogs'
+export * from './srp'
 
 export type Dict<T=any> = Record<string, T>
 export type Falsy = false | null | undefined | '' | 0
@@ -70,17 +70,5 @@ export function setHidden(dest: object, src:object) {
         writable: true,
         value,
     })))
-}
-
-export async function srpSequence(username:string, password:string, apiCall: (cmd:string, params:any) => any) {
-    const { pubKey, salt } = await apiCall('loginSrp1', { username })
-    if (!salt) throw Error('salt')
-    const srp6aNimbusRoutines = new SRPRoutines(new SRPParameters())
-    const srp = new SRPClientSession(srp6aNimbusRoutines);
-    const resStep1 = await srp.step1(username, password)
-    const resStep2 = await resStep1.step2(BigInt(salt), BigInt(pubKey))
-    const res = await apiCall('loginSrp2', { pubKey: String(resStep2.A), proof: String(resStep2.M1) }) // bigint-s must be cast to string to be json-ed
-    await resStep2.step3(BigInt(res.proof)).catch(() => Promise.reject('trust'))
-    return res
 }
 
