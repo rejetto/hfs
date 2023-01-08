b2f2b27a190c2c8399c433b5bac75d8db65e707a
Massimo Melina <a@rejetto.com>
2023-01-08 01:03:41 +0100

    plugin/vhosting: support masks for domain

--- plugins/vhosting/plugin.js
+++ plugins/vhosting/plugin.js
@@ -1,5 +1,5 @@
 exports.description = "If you want to have different home folders, based on domain"
-exports.version = 2 // added config.mandatory
+exports.version = 3 // support masks for host
 exports.apiRequired = 2 // 2 is for the config 'array'
 
 exports.config = {
@@ -7,7 +7,7 @@ exports.config = {
         label: '',
         type: 'array',
         fields: {
-            host: { label: "Domain" },
+            host: { label: "Domain", helperText: "Masks supported: domain.*|other.*" },
             root: { helperText: "Root path in VFS" },
         }
     },
@@ -17,24 +17,27 @@ exports.config = {
 	}
 }
 
-exports.init = api => ({
-    middleware(ctx) {
-        let toModify = ctx
-        if (ctx.path.startsWith(api.const.SPECIAL_URI)) { // special uris should be excluded...
-            toModify = ctx.params
-            if (toModify.path === undefined) // ...unless they carry a path in the query. In that case we'll work that.
-                return
-        }
-        const hosts = api.getConfig('hosts')
-        if (!hosts?.length) return
-        for (const row of hosts)
-            if (ctx.host === row.host) {
-                toModify.path = row.root + toModify.path
-                return
+exports.init = api => {
+    const { isMatch } = api.require('micromatch')
+    return {
+        middleware(ctx) {
+            let toModify = ctx
+            if (ctx.path.startsWith(api.const.SPECIAL_URI)) { // special uris should be excluded...
+                toModify = ctx.params
+                if (toModify.path === undefined) // ...unless they carry a path in the query. In that case we'll work that.
+                    return
             }
-        if (api.getConfig('mandatory')) {
-            ctx.socket.destroy()
-            return true
+            const hosts = api.getConfig('hosts')
+            if (!hosts?.length) return
+            for (const row of hosts)
+                if (isMatch(ctx.host, row.host)) {
+                    toModify.path = row.root + toModify.path
+                    return
+                }
+            if (api.getConfig('mandatory')) {
+                ctx.socket.destroy()
+                return true
+            }
         }
     }
-})
+}
