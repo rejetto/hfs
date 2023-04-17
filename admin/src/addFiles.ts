22403f15d8fa52af58fcf8649031cd4b1f7376b9
Massimo Melina <a@rejetto.com>
2023-04-17 12:52:38 +0200

    admin/fs: add from disk now starts from the parent source, if any

--- admin/src/addFiles.ts
+++ admin/src/addFiles.ts
@@ -3,7 +3,7 @@
 import { alertDialog, newDialog, promptDialog } from './dialog'
 import { createElement as h, Fragment } from 'react'
 import { Box } from '@mui/material'
-import { VfsNode, reloadVfs } from './VfsPage'
+import { reloadVfs } from './VfsPage'
 import { state } from './state'
 import { apiCall } from './api'
 import FilePicker from './FilePicker'
@@ -17,11 +17,12 @@ export default function addFiles() {
             const parent = getParent()
             return h(Fragment, {},
                 h(Box, { sx:{ typography: 'body1', px: 1, py: 2 } },
-                    "Selected elements will be added under " + (parent || '(home)')),
+                    "Selected elements will be added under " + (parent.isRoot ? '(home)' : parent.id)),
                 h(FilePicker, {
+                    from: parent.source,
                     async onSelect(sel) {
                         const errs = onlyTruthy(await Promise.all(sel.map(source =>
-                            apiCall('add_vfs', { parent, source }).then(() => null, e => [source,e.message]) )))
+                            apiCall('add_vfs', { parent: parent.id, source }).then(() => null, e => [source, e.message]))))
                         if (errs.length)
                             await alertDialog(h(Box, {},
                                 "Some elements have been rejected",
@@ -43,9 +44,9 @@ export async function addVirtual() {
     try {
         const name = await promptDialog("Enter folder name")
         if (!name) return
-        const parent = getParent()
+        const { id: parent } = getParent()
         await apiCall('add_vfs', { parent, name })
-        reloadVfs([ (parent||'') + '/' + name ])
+        reloadVfs([ parent + name ])
         await alertDialog(`Folder "${name}" created`, 'success')
     }
     catch(e) {
@@ -54,10 +55,6 @@ export async function addVirtual() {
 }
 
 function getParent() {
-    let f: VfsNode | undefined = state.selectedFiles[0]
-    if (!f || f.isRoot)
-        return ''
-    if (f.type !== 'folder')
-        f = f.parent
-    return f?.id || ''
+    const f = state.selectedFiles[0] || state.vfs
+    return f.type === 'folder' ? f : f.parent!
 }
