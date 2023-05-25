edd6cea6740f9957e8e48425f3d28663392f1e8b
Massimo Melina <a@rejetto.com>
2023-05-25 22:24:50 +0200

    dev: watch-server-proxied not working anymore

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
     ],
     server: {
         port: 3006,
+        host: '127.0.0.1',
         proxy: {
             '/~/': {
                 target: 'http://localhost',
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     },
     server: {
         port: 3005,
+        host: '127.0.0.1',
         proxy: {
             '/~/': {
                 target: 'http://localhost',
