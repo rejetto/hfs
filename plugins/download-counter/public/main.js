904725d9047b9df4ad8314bfd0d1b03115b9a846
Massimo Melina <a@rejetto.com>
2023-06-28 22:23:50 +0200

    fix: download counter in file menu was not translated

--- plugins/download-counter/public/main.js
+++ plugins/download-counter/public/main.js
@@ -1,12 +1,13 @@
 { // this wrapper avoids name clashing of outer variables and functions
     const config = HFS.getPluginConfig()
 
+    const label = HFS.t(["Download counter", "download counter"])
     const inMenu = config.where === 'menu'
     HFS.onEvent('additionalEntryDetails', ({ entry: { hits } }) =>
-        hits && !inMenu && `<span class="download-counter" title="${HFS.t`download counter`}">${hits}</span>`)
+        hits && !inMenu && `<span class="download-counter" title="${label}">${hits}</span>`)
 
     HFS.onEvent('fileMenu', ({ entry, props }) => {
         if (inMenu && !entry.isFolder)
-            props.push(["Downloads", entry.hits || 0])
+            props.push([label, entry.hits || 0])
     })
 }
