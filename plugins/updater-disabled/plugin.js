c5a361b78f3aef3bc365f5359bcc388f96f643b3
Massimo Melina <a@rejetto.com>
2023-08-20 15:22:21 +0200

    fix: plugin/updater: can fail if update happens while the new file is uploading

--- plugins/updater-disabled/plugin.js
+++ plugins/updater-disabled/plugin.js
@@ -5,11 +5,12 @@ Its capability is to offer automatic restart when an update is available in the 
 Tip: In my case I'm uploading "hfs.exe-new" over ftp, and it's a bit slow, so to avoid the plugin to catch
     an unfinished file, I do the upload using a temporary name, and only when it's done I rename it to "hfs.exe-new".
 */
-exports.version = 0.2
+exports.version = 0.3
 exports.description = "automatic restart when an update is available in the form of hfs.exe-new file"
 exports.apiRequired = 3 // api.log
 
 const NEW = 'hfs.exe-new'
+const UPDATING = 'hfs.exe-updating'
 const BATCH_NAME = 'run-updater.bat'
 const BATCH = `@echo off
 :loop
@@ -19,7 +20,7 @@ hfs %*
 endlocal
 if exist hfs.exe-new (
   move /y hfs.exe hfs.exe-old
-  move /y ${NEW} hfs.exe
+  move /y ${UPDATING} hfs.exe
   goto loop  
 )
 `
@@ -33,7 +34,7 @@ exports.init = async api => {
 
     api.log("ready")
     const timer = setInterval(() => {
-        fs.access(NEW, fs.constants.W_OK, err => {
+        fs.rename(NEW, UPDATING, err => {
             if (err) return
             api.log("exiting for update")
             process.exit(0)
