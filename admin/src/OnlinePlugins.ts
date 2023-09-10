e3f76da591abf8e6337d59396171d02ac47ef258
Massimo Melina <a@rejetto.com>
2023-09-10 20:20:18 +0200

    admin/plugins: ask confirmation to start a plugin that was just downloaded

--- admin/src/OnlinePlugins.ts
+++ admin/src/OnlinePlugins.ts
@@ -9,12 +9,12 @@ import { StringField } from '@hfs/mui-grid-form'
 import { useDebounce } from 'usehooks-ts'
 import { renderName, showError, startPlugin } from './InstalledPlugins'
 import { state, useSnapState } from './state'
-import { alertDialog } from './dialog'
+import { alertDialog, confirmDialog } from './dialog'
 
 export default function OnlinePlugins() {
     const [search, setSearch] = useState('')
     const debouncedSearch = useDebounce(search, 1000)
-    const { list, error, initializing, updateList } = useApiList('get_online_plugins', { text: debouncedSearch })
+    const { list, error, initializing } = useApiList('get_online_plugins', { text: debouncedSearch })
     const snap = useSnapState()
     if (error)
         return showError(error)
@@ -78,7 +78,8 @@ export default function OnlinePlugins() {
                         const branch = row.branch || row.default_branch
                         try {
                             const res = await apiCall('download_plugin', { id, branch }, { timeout: false })
-                            await startPlugin(res.id)
+                            if (await confirmDialog(`Plugin ${id} downloaded`, { confirmText: "Start" }))
+                                await startPlugin(res.id)
                         }
                         catch(e: any) {
                             if (e.code !== 424) throw e
