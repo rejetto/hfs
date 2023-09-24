9091be136f9d4b29f9c2e72146d4ec76cfb6fd7d
Massimo Melina <a@rejetto.com>
2023-09-24 14:20:47 +0200

    dev: avoid harmless warnings while building https://github.com/TanStack/query/issues/5175

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -18,7 +18,10 @@ export default defineConfig({
                     },
                 ])
             ],
-
+            onwarn(warning, warn) {
+                if (warning.code === 'MODULE_LEVEL_DIRECTIVE' && warning.message.includes(`"use client"`)) return
+                warn(warning)
+            },
         }
     },
     server: {
