dcdecf59983d7f1b744c1b5f2430be8566aa03c4
Massimo Melina <a@rejetto.com>
2023-11-30 21:56:35 +0100

    admin/internet: generate certificate with multiple domains

--- src/api.net.ts
+++ src/api.net.ts
@@ -1,11 +1,12 @@
 // This file is part of HFS - Copyright 2021-2023, Massimo Melina <a@rejetto.com> - License https://www.gnu.org/licenses/gpl-3.0.txt
 
 import { ApiError, ApiHandlers } from './apiMiddleware'
-import { HTTP_FAILED_DEPENDENCY, HTTP_SERVER_ERROR, HTTP_SERVICE_UNAVAILABLE, HTTP_PRECONDITION_FAILED } from './const'
+import { HTTP_FAILED_DEPENDENCY, HTTP_SERVER_ERROR, HTTP_SERVICE_UNAVAILABLE, HTTP_PRECONDITION_FAILED, HTTP_NOT_FOUND
+} from './const'
 import _ from 'lodash'
 import { getCertObject } from './listen'
 import { getProjectInfo } from './github'
-import { apiAssertTypes, objSameKeys, onlyTruthy, promiseBestEffort } from './misc'
+import { apiAssertTypes, onlyTruthy, promiseBestEffort } from './misc'
 import { lookup, Resolver } from 'dns/promises'
 import { isIPv6 } from 'net'
 import { getNatInfo, upnpClient } from './nat'
@@ -70,13 +71,13 @@ const apis: ApiHandlers = {
         return results.length ? results : new ApiError(HTTP_SERVICE_UNAVAILABLE)
     },
 
-    async make_cert({domain, email}) {
-        await makeCert(domain, email)
+    async make_cert({domain, email, altNames}) {
+        await makeCert(domain, email, altNames)
         return {}
     },
 
     get_cert() {
-        return objSameKeys(_.pick(getCertObject(), ['subject', 'issuer', 'validFrom', 'validTo']), v => v)
+        return getCertObject() || new ApiError(HTTP_NOT_FOUND)
     }
 }
 
