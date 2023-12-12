1c40ae58004c7abcb1007e8b028a8c5a948677fb
Massimo Melina <a@rejetto.com>
2023-12-12 19:56:21 +0100

    fix: new address for beta versions

--- src/const.ts
+++ src/const.ts
@@ -16,7 +16,7 @@ export const BUILD_TIMESTAMP = fs.statSync(PKG_PATH).mtime.toISOString()
 const pkg = JSON.parse(fs.readFileSync(PKG_PATH,'utf8'))
 export const VERSION = pkg.version
 export const RUNNING_BETA = VERSION.includes('-')
-export const HFS_REPO_BRANCH = RUNNING_BETA ? 'next' : 'main'
+export const HFS_REPO_BRANCH = RUNNING_BETA ? VERSION.split('.')[1] : 'main'
 export const IS_WINDOWS = process.platform === 'win32'
 export const IS_MAC = process.platform === 'darwin'
 export const IS_BINARY = !basename(process.execPath).includes('node') // this won't be node if pkg was used
