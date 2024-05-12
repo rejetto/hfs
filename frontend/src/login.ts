3cc5cd25c8ad72ce967f4db5a35a43fe240476f1
Massimo Melina <a@rejetto.com>
2024-05-12 19:08:37 +0200

    fix: still faulty account-redirect

--- frontend/src/login.ts
+++ frontend/src/login.ts
@@ -115,7 +115,7 @@ export async function loginDialog(closable=false) {
                         close(true)
                         if (res?.redirect)
                             setTimeout(() => // workaround: the history.back() issued by closing the dialog is messing with our navigation
-                                getHFS().navigate(res.redirect) )
+                                getHFS().navigate(res.redirect), 10) // from my tests 1 was enough, 0 was not (not always). Would be nice to find a cleaner way
                     } catch (err: any) {
                         await alertDialog(err)
                         usrRef.current?.focus()
