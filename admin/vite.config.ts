5ab40b0e1216898df5c6da8215508f32539ad2fb
Massimo Melina <a@rejetto.com>
2024-07-07 18:08:54 +0200

    disabled vite-plugin-babel-import, as the gain is small (0.5s on my machine) and is causing problems on linux (possibly because of libraryChangeCase value)

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
-import vitePluginImport from 'vite-plugin-babel-import';
+//import vitePluginImport from 'vite-plugin-babel-import';
+// package.json/devDependencies: "vite-plugin-babel-import": "github:rejetto/vite-plugin-babel-import"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +9,7 @@ export default defineConfig({
         emptyOutDir: true,
         target: "es2015",
         rollupOptions: {
-            plugins: [
+       /*     plugins: [
                 vitePluginImport([
                     { // speed up build process (~2s on my M1) by bringing "modules transformed" from 11k+ down to 1.7k+
                         libraryName: '@mui/icons-material',
@@ -17,7 +18,7 @@ export default defineConfig({
                         ignoreStyles: [],
                     },
                 ])
-            ],
+            ],*/
             onwarn(warning, warn) {
                 if (warning.code === 'MODULE_LEVEL_DIRECTIVE' && warning.message.includes(`"use client"`)) return
                 warn(warning)
@@ -29,7 +30,8 @@ export default defineConfig({
         host: '127.0.0.1',
         proxy: {
             '/~/': {
-                target: 'http://localhost',
+                target:
+                    'http://localhost',
                 proxyTimeout: 2000,
                 changeOrigin: true,
                 ws: true,
