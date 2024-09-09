c4a8d89098b390a8c69c7339b617e3592c36664e
Massimo Melina <a@rejetto.com>
2024-09-09 16:02:54 +0200

    langs/tr

--- src/langs/embedded.ts
+++ src/langs/embedded.ts
@@ -15,5 +15,6 @@ import de from './hfs-lang-de.json'
 import fi from './hfs-lang-fi.json'
 import hu from './hfs-lang-hu.json'
 import ja from './hfs-lang-ja.json'
+import tr from './hfs-lang-tr.json'
 
-export default { it, zh, ru, sr, ko, ms, 'zh-tw': zh_tw, fr, 'pt-br': pt_br, vi, es, nl, el, de, fi, hu, ja }
+export default { it, zh, ru, sr, ko, ms, 'zh-tw': zh_tw, fr, 'pt-br': pt_br, vi, es, nl, el, de, fi, hu, ja, tr }
