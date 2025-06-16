c34c9c8a4d0d794dee11815e69842d02642bbdb0
Massimo Melina <a@rejetto.com>
2025-06-16 19:25:55 +0200

    langs/lt #1022

--- src/langs/embedded.ts
+++ src/langs/embedded.ts
@@ -20,5 +20,6 @@ import tr from './hfs-lang-tr.json'
 import th from './hfs-lang-th.json'
 import uk from './hfs-lang-uk.json'
 import ar from './hfs-lang-ar.json'
+import lt from './hfs-lang-lt.json'
 
-export default { it, zh, ru, sr, ko, ms, 'zh-tw': zh_tw, fr, 'pt-br': pt_br, vi, es, nl, el, de, fi, hu, ja, ro, tr, th, uk, ar }
+export default { it, zh, ru, sr, ko, ms, 'zh-tw': zh_tw, fr, 'pt-br': pt_br, vi, es, nl, el, de, fi, hu, ja, ro, tr, th, uk, ar, lt }
