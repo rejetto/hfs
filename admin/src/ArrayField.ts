227a150e2d9c524b37ffeb53ce9359f0d524a7a9
Massimo Melina <a@rejetto.com>
2025-06-26 00:30:26 +0200

    dx: support objects for ArrayField.value

--- admin/src/ArrayField.ts
+++ admin/src/ArrayField.ts
@@ -1,18 +1,15 @@
-// This file is part of HFS - Copyright 2021-2023, Massimo Melina <a@rejetto.com> - License https://www.gnu.org/licenses/gpl-3.0.txt
-
-import { createElement as h, Fragment, useMemo, useState, isValidElement } from 'react'
-import { callable, DialogOptions, Dict, Functionable, isOrderedEqual, setHidden, swap } from './misc'
+import { createElement as h, Fragment, isValidElement, useMemo, useState } from 'react'
+import { callable, Dict, Functionable, isOrderedEqual, setHidden, swap } from './misc'
 import { Add, Edit, Delete, ArrowUpward, ArrowDownward, Undo, Check } from '@mui/icons-material'
-import { FormDialog, formDialog } from './dialog'
+import { DialogOptions, FormDialog, formDialog } from './dialog'
 import { GridActionsCellItem, GridAlignment, GridColDef } from '@mui/x-data-grid'
 import { BoolField, FieldDescriptor, FieldProps, labelFromKey } from '@hfs/mui-grid-form'
 import { Box, FormHelperText, FormLabel } from '@mui/material'
-import { DateTimeField } from './DateTimeField'
 import _ from 'lodash'
 import { Center, Flex, IconBtn, useBreakpoint } from './mui'
 import { DataTable, DataTableColumn } from './DataTable'
 
-type ArrayFieldProps<T> = FieldProps<T[]> & {
+type ArrayFieldProps<T> = FieldProps<T[] | Dict<T>> & {
     fields: Functionable<FieldDescriptor[] & {
         $width?: number,
         $column?: Partial<DataTableColumn>,
@@ -29,20 +26,22 @@ type ArrayFieldProps<T> = FieldProps<T[]> & {
     form?: Partial<FormDialog<any>>,
     dialog?: Partial<DialogOptions>,
     details?: boolean
+    objectK?: string
 }
-export function ArrayField<T extends object>({ label, helperText, fields, value, onChange, onError, setApi, reorder, prepend, noRows, valuesForAdd, autoRowHeight, dialog, form, details, ...rest }: ArrayFieldProps<T>) {
-    if (!Array.isArray(value)) // avoid crash if non-array values are passed, especially developing plugins
-        value = []
-    const rows = useMemo(() => value!.map((x,$idx) =>
+export function ArrayField<T extends object>({ label, helperText, fields, value, onChange, onError, setApi, reorder, prepend, noRows, valuesForAdd, autoRowHeight, dialog, form, details, objectK, ...rest }: ArrayFieldProps<T>) {
+    const valueA = Array.isArray(value) ? value
+        : !objectK || !value ? [] // avoid crash if non-array values are passed, especially developing plugins
+            : Object.entries(value).map(([k, v]) => ({ [objectK]: k, ...v }))
+    const rows = useMemo(() => valueA!.map((x,$idx) =>
             setHidden({ ...x } as any, x.hasOwnProperty('id') ? { $idx } : { id: $idx })),
-        [JSON.stringify(value)]) //eslint-disable-line
+        [JSON.stringify(valueA)]) //eslint-disable-line
     const formProp = (values: any) => ({
         fields: callable(fields, values).map(({ $width, $column, $type, $hideUnder, showIf, $render, $mergeRender, ...rest }) =>
             (!showIf || showIf(values)) && _.defaults(rest, byType[$type]?.field)),
         ...form,
     })
     setApi?.({ isEqual: isOrderedEqual }) // don't rely on stringify, as it wouldn't work with non-json values
-    const [undo, setUndo] = useState<typeof value>()
+    const [undo, setUndo] = useState<typeof valueA>()
     return h(Fragment, {},
         h(Flex, { rowGap: 0, flexWrap: 'wrap' },
             label && h(FormLabel, { sx: { ml: .5 } }, label),
@@ -102,7 +101,7 @@ export function ArrayField<T extends object>({ label, helperText, fields, value,
                                     onClick: ev =>
                                         formDialog<T>({ form: formProp, title, values: valuesForAdd, dialogProps: dialog }).then(x => {
                                             if (!x) return
-                                            const newValue = value?.slice() || []
+                                            const newValue = valueA.slice() || []
                                             if (prepend) newValue.unshift(x)
                                             else newValue.push(x)
                                             set(newValue, ev)
@@ -133,7 +132,7 @@ export function ArrayField<T extends object>({ label, helperText, fields, value,
                                             dialogProps: dialog
                                         })
                                         if (res)
-                                            set(value!.map((oldRec, i) => i === $idx ? res : oldRec), ev)
+                                            set(valueA.map((oldRec, i) => i === $idx ? res : oldRec), ev)
                                     }
                                 }),
                                 h(GridActionsCellItem as any, {
@@ -142,7 +141,7 @@ export function ArrayField<T extends object>({ label, helperText, fields, value,
                                     showInMenu: reorder,
                                     onClick: ev => {
                                         ev.stopPropagation()
-                                        set(value!.filter((_rec, i) => i !== $idx), ev)
+                                        set(valueA.filter((_rec, i) => i !== $idx), ev)
                                     },
                                 }),
                                 reorder && $idx && h(GridActionsCellItem as any, {
@@ -151,7 +150,7 @@ export function ArrayField<T extends object>({ label, helperText, fields, value,
                                     showInMenu: true,
                                     onClick: ev => {
                                         ev.stopPropagation()
-                                        set(swap(value!.slice(), $idx, $idx - 1), ev)
+                                        set(swap(valueA.slice(), $idx, $idx - 1), ev)
                                     },
                                 }),
                                 reorder && $idx < rows.length - 1 && h(GridActionsCellItem as any, {
@@ -160,7 +159,7 @@ export function ArrayField<T extends object>({ label, helperText, fields, value,
                                     showInMenu: true,
                                     onClick: ev => {
                                         ev.stopPropagation()
-                                        set(swap(value!.slice(), $idx, $idx + 1), ev)
+                                        set(swap(valueA.slice(), $idx, $idx + 1), ev)
                                     },
                                 }),
                             ].filter(Boolean)
@@ -171,9 +170,10 @@ export function ArrayField<T extends object>({ label, helperText, fields, value,
         )
     )
 
-    function set(newValue: NonNullable<typeof value>, event?: any) {
-        onChange(newValue, { was: value, event })
-        setUndo(value)
+    function set(newValue: typeof valueA, event?: any) {
+        onChange(!objectK ? newValue : Object.fromEntries((newValue as any).map(({ [objectK]: k, ...v }) => [k, v])),
+            { was: value, event })
+        setUndo(valueA)
     }
 
 }
@@ -184,7 +184,6 @@ const byType: Dict<{ field?: Partial<FieldDescriptor>, column?: Partial<GridColD
         column: { renderCell: ({ value }) => value && h(Check) },
     },
     dateTime: {
-        field: { comp: DateTimeField },
         column: {
             minWidth: 96, flex: 0.5,
             renderCell: ({ value }) => value && new Date(value).toLocaleString(),
