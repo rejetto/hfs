5ffb352c338f192884e01dc4916993257c08cf59
Massimo Melina <a@rejetto.com>
2025-09-26 09:56:59 +0200

    better english

--- admin/src/FileForm.ts
+++ admin/src/FileForm.ts
@@ -155,8 +155,12 @@ export default function FileForm({ file, addToBar, statusApi, accounts, saved }:
         fields: [
             isRoot ? h(Alert, { severity: 'info' }, "This is Home, the root of your shared files. Options set here will be applied to all files.")
                 : isDir && hasSource && h(Alert, { severity: 'info' }, `To set permissions on individual items in folder, add them by clicking Add button, and then "from disk"`),
-            !isRoot && { k: 'name', required: true, xl: true, helperText: hasSource && "You can decide a name that's different from the one on your disk",
-                end: nameFromSource && !nameIsDerivedFromSource && h(Btn, { icon: RestartAlt, title: "Reset", onClick: () => setValues({ ...values, name: nameFromSource }) }),
+            !isRoot && {
+                k: 'name', required: true, xl: true, helperText: hasSource && "You can decide a name that's different from the one on your disk",
+                end: nameFromSource && !nameIsDerivedFromSource && h(Btn, {
+                    icon: RestartAlt, title: "Reset to same name on disk",
+                    onClick: () => setValues({ ...values, name: nameFromSource })
+                }),
             },
             isLink ? { k: 'url', label: "URL", lg: 12, required: true }
                 : { k: 'source', label: "Disk source", xl: true, comp: FileField, files: isUnknown || !isDir, folders: isUnknown || isDir,
