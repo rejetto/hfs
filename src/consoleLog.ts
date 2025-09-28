2d975a82974f9f150eb3b1120238aed2e0faa197
Massimo Melina <a@rejetto.com>
2025-09-28 12:09:52 +0200

    better output for --debug

--- src/consoleLog.ts
+++ src/consoleLog.ts
@@ -5,19 +5,23 @@ import { argv } from './argv'
 
 export const consoleLog: Array<{ ts: Date, k: string, msg: string }> = []
 const f = argv.consoleFile ? createWriteStream(argv.consoleFile, 'utf-8') : null
-for (const k of ['log','warn','error']) {
+for (const k of ['log','warn','error','debug']) {
     const original = console[k as 'log']
     console[k as 'log'] = (...args: any[]) => {
         const ts = new Date()
-        const msg = safeJoin(args) // if args contains a symbol, join will throw
-        const rec = { ts, k, msg }
-        consoleLog.push(rec)
-        if (consoleLog.length > 100_000) // limit to avoid infinite space
-            consoleLog.splice(0, 1_000)
-        events.emit('console', rec)
-        f?.write(`${formatTimestamp(ts)} [${k}] ${msg}\n`)
-        if (k !== 'log')
-            args.unshift('!')
+        if (k === 'debug')
+            args.unshift('DBG')
+        else {
+            const msg = safeJoin(args) // if args contains a symbol, join will throw
+            const rec = { ts, k, msg }
+            consoleLog.push(rec)
+            if (consoleLog.length > 100_000) // limit to avoid infinite space
+                consoleLog.splice(0, 1_000)
+            events.emit('console', rec)
+            f?.write(`${formatTimestamp(ts)} [${k}] ${msg}\n`)
+            if (k !== 'log')
+                args.unshift('!')
+        }
         return original(formatTime(ts), ...args) // bundled nodejs doesn't have locales (and apparently uses en-US)
     }
 }
