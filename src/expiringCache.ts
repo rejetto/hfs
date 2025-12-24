b8f3aa20a0c214b05e162169a8fd6cf4b071e33f
Massimo Melina <a@rejetto.com>
2025-12-24 14:20:23 +0100

    better code: improved expiringCache

--- src/expiringCache.ts
+++ src/expiringCache.ts
@@ -1,13 +1,24 @@
 export function expiringCache<T, K=string>(ttlMs: number) {
+    if (!ttlMs)
+        throw Error('invalid TTL')
     const o = new Map<K,T>()
     return Object.assign(o, {
-        try(k: K, creator: () => T): T {
+        // creator can return undefined if the value should not be cached. `invalidate` is useful in case you have some custom logic for it, other than ttl.
+        try(k: K, creator: (invalidate: () => void) => T): T {
             let ret = o.get(k)
-            if (ret === undefined) {
-                ret = creator()
-                o.set(k, ret)
-                // in case of async, wait for it to be done before starting the timer
-                Promise.resolve(ret).finally(() => setTimeout(() => o.delete(k), ttlMs)).catch(() => {})
+            if (ret === undefined) { // undefined = missing, as we don't accept this value in our cache
+                ret = creator(invalidate)
+                if (ret !== undefined) {
+                    o.set(k, ret)
+                    Promise.resolve(ret).then(v => {
+                        if (v === undefined) // even in a promise, we'll consider undefined as a request to cancel the caching
+                            invalidate()
+                    }, () => {}) // avoid js warning
+                        .finally(() => setTimeout(invalidate, ttlMs)) // wait for async (in case) before starting the timer
+                }
+                function invalidate() {
+                    o.delete(k)
+                }
             }
             return ret
         },
