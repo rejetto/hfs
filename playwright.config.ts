75dee6cd5a44eef783316c31169500f23c47a3a3
Massimo Melina <a@rejetto.com>
2026-04-10 00:36:07 +0200

    dx: collect info on ui-tests failure

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,6 +29,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
     timezoneId: 'Europe/Rome',
   },
 
