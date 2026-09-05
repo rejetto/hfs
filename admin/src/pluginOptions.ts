ea24a23c1553877f723df466fb30724829636564
Massimo Melina <a@rejetto.com>
2026-09-05 13:13:54 +0200

    fix: (regression beta) admin/plugins/installed: in options dialog, the save button was not working

--- admin/src/pluginOptions.ts
+++ admin/src/pluginOptions.ts
@@ -6,7 +6,7 @@ import { Btn, Flex, iconTooltip, NetmaskField } from './mui'
 import { MilitaryTech, Clear, Group } from '@mui/icons-material'
 import { Html, md, replaceStringToReact, useAutoScroll } from '@hfs/shared'
 import {
-    BoolField, Field, FieldProps, MultiSelectField, NumberField, SelectField, StringField, FormApi
+    BoolField, Field, FieldProps, MultiSelectField, NumberField, SelectField, StringField
 } from '@hfs/mui-grid-form'
 import { ArrayField } from './ArrayField'
 import _ from 'lodash'
@@ -23,24 +23,18 @@ export async function showPluginOptions(row: any, maxWidth: string) {
     const { config: lastSaved } = await apiCall('get_plugin', { id })
     // array fields contain a DataGrid whose intrinsic width settles in steps, so give the form a stable preferred width
     const workaround = _.some(callable(row.config, lastSaved), { type: 'array' }) ? `min(100%, ${maxWidth})` : undefined
-    const apiRef = { current: undefined as FormApi | undefined }
     // support css values without having to wrap in sx, as in DialogProps it only supports breakpoints
     const showOptions = Boolean(row.config)
     const values = await formDialog({
         title: showOptions ? `Options for ${id}` : `Log for ${id}`,
-        form: values => ({
+        form: (values, { submit }) => ({
             before: row.description && h(Box, { sx: { mx: 2, mb: 2 } }, row.description),
             fields: makeFields(callable(row.config, values) || {}, values),
             save: showOptions ? { children: "Save and close" } : false,
             barSx: { gap: 1 },
-            apiRef,
             addToBar: [h(Btn, {
                 variant: 'outlined',
-                async onClick() {
-                    // this action must reuse form validation without falling through to the dialog-closing submit path
-                    if (await apiRef.current?.validate())
-                        await save(values)
-                }
+                onClick: () => submit(save),
             }, "Save")],
         }),
         values: lastSaved,
